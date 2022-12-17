31a24bb39145653af21a60fa9073725b585ab248
JonaThicke274 <lipatajonathan274@gmail.com>
2022-12-17 14:19:49 -0500

    deinfed query and mutation functionality for gql/mongoose models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,10 +4,58 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id})
+                    .select('-__v -password')
+                    .populate('savedBooks')
+                return userData;
+            }
+        }
     },
     Mutation: {
+        createUser: async(parents, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user };
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const userSavedBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: args }},
+                    { new: true }
+                )
+                return userSavedBook;
+            }
+        },
+        deleteBook: async (parent, args, context) => {
+            if (context.user) {
+                const userDeletedBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } }},
+                    { new: true }
+                );
+                return userDeletedBook;
+            }
+        }
     }
 };
 
